c11912589ca1378ce9f9202704f1d060ff26ee4c
Marie Schmidt <marieulrikeschmidt@gmail.com>
2019-07-19 15:03:28 -0500

    Update dependency.Minor changes to CustomerComponent & Customer Template files.

--- CustomerApplication/src/CustomerApp/Customer/CustomerApp.CustomerComponent.ts
+++ CustomerApplication/src/CustomerApp/Customer/CustomerApp.CustomerComponent.ts
@@ -13,4 +13,14 @@ export class CustomerComponent {
     this.CustomerModels.push(this.CustomerModel); /*pushes a new customer into the UI table */
     this.CustomerModel = new Customer(); /*clears the UI, so user doesn't have to delete the form*/
   }
+
+/* (This portion threw errors in the console that I wasn't able to resolve yet. It also grayes out the Add Button.
+NOT using this atm)--> More comments related to code below in CustomerView.html lines 18-21)
+*/
+/*
+  hasError(typeofvalidator: string,
+           controlname: string): boolean {
+            return this.CustomerModel.formCustomerGroup.contains[controlname].hasError(typeofvalidator);
+    }
+    */
 }
